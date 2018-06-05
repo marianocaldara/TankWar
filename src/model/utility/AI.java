c75d0d77e6d700a9f7fbcd8614e300eefde75730
Filippo <filippo.brintazzoli@studio.unibo.it>
2018-06-05 09:33:27 +0200

    Implemented goAway() and goCloser() and added javadoc

--- src/model/utility/AI.java
+++ src/model/utility/AI.java
@@ -6,32 +6,44 @@ import java.util.Map;
 import model.input.InputImpl;
 import model.object.Tank;
 
+/**
+ * 
+ * Class AI implements methods to make enemy tank act like a player
+ *
+ */
 public class AI {
-    
+
     private static Map<Direction, Boolean> movement = new HashMap<>();
-    private static InputImpl input;
-    
-    public static void act(Tank enemy, Tank player) {
-        
+
+    /**
+     * 
+     * @param enemy
+     *  the enemy tank, the one to be moved by IA
+     * @param player
+     *  the player tank, the one to be followed by enemy
+     * @return
+     *  the the input for the enemy tank
+     */
+    public static InputImpl act(Tank enemy, Tank player) {
+
         if (Calculate.distance(enemy.getPosition(), player.getPosition()) > 200) {
             goCloser(enemy, player);
         } else {
             goAway(enemy, player);
         }
-        
-        input = new InputImpl(movement, null);  // Manca metodo per cannone + shot, quindi per ora null
-        
-        // Probabilmente in realtà la act ritorna l'input e la enemy.update va chiamata nel controller
-        // ma per ora la metto qua e lascio void act
-        enemy.update(input);
+
+        return new InputImpl(movement, player.getPosition());
     }
-    
-    private static void goAway(Tank enemy, Tank player) {
-        // Vedere se si può chiamare goCloser e poi semplicemente invertire velocità
-    }
-    
+
+    /**
+     * Move the enemy tank closer to the player tank
+     * 
+     * @param enemy
+     *          the enemy tank, the one to be moved by IA
+     * @param player
+     *          the player tank, the one to be followed by enemy
+     */
     private static void goCloser(Tank enemy, Tank player) {
-        
         // RIGHT and LEFT
         if (enemy.getPosition().getFirst() - player.getPosition().getFirst() > 0) {
             movement.put(Direction.LEFT, true);
@@ -40,7 +52,6 @@ public class AI {
             movement.put(Direction.RIGHT, true);
             movement.put(Direction.LEFT, false);
         }
-        
         // UP and DOWN
         if (enemy.getPosition().getSecond() - player.getPosition().getSecond() > 0) {
             movement.put(Direction.UP, true);
@@ -50,5 +61,25 @@ public class AI {
             movement.put(Direction.UP, false);
         }
     }
-    
+
+    /**
+     * Move the enemy tank away from the player tank
+     * 
+     * @param enemy
+     *          the enemy tank, the one to be moved by IA
+     * @param player
+     *          the player tank, the one to be followed by enemy
+     */
+    private static void goAway(Tank enemy, Tank player) {
+        // Call goCloser() and then invert boolean values to make the opposite movement
+        goCloser(enemy, player);
+        movement.forEach((d,b) -> {
+            if (b == true) {
+                b = false;
+            } else {
+                b = true;
+            }
+        });
+    }
+
 }
